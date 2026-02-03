69dbcda6da5610d44fd2048097c858c3a2dc115f
Camila Basualdo Cibils <camila.basualdocibils@gmail.com>
2026-02-03 13:37:34 -0800

    some fdix idk

--- cpp/src/Singleton.hpp
+++ cpp/src/Singleton.hpp
@@ -1,50 +1,56 @@
 #pragma once
 #include <mutex>
 #include <memory>
-
 template <typename Type>
 class Singleton
 {
 private:
-    static inline std::mutex GetMutex;
+    static inline std::mutex Mutex;
     static inline std::unique_ptr<Type> Instance;
 
+protected:
+    Singleton() = default;
+
 public:
-    Singleton() = delete;
     Singleton(const Singleton&) = delete;
     Singleton& operator=(const Singleton&) = delete;
 
+    // Default Get (only works if default-constructible)
+    [[nodiscard]] static Type& Get()
+    {
+        static_assert(
+            std::is_default_constructible_v<Type>,
+            "Type is not default-constructible. Call Get(args...) instead."
+        );
+
+        std::lock_guard lock(Mutex);
+        if (!Instance)
+        {
+            Instance = std::make_unique<Type>();
+        }
+        return *Instance;
+    }
+
+    // Argument-inferred Get
     template <typename... Args>
-    static Type& Make(Args&&... args)
+    [[nodiscard]] static Type& Get(Args&&... args)
     {
         static_assert(
             std::is_constructible_v<Type, Args...>,
             "Type is not constructible with the given arguments"
         );
 
-        std::lock_guard lock(GetMutex);
-        ASSERT(!Instance, "Singleton already exists");
-
-        Instance = std::make_unique<Type>(std::forward<Args>(args)...);
-        return *Instance;
-    }
-
-    [[nodiscard]] static Type& Get()
-    {
-        std::lock_guard lock(GetMutex);
-        ASSERT(Instance, "Singleton not created yet");
+        std::lock_guard lock(Mutex);
+        if (!Instance)
+        {
+            Instance = std::make_unique<Type>(std::forward<Args>(args)...);
+        }
         return *Instance;
     }
 
     static bool Exists()
     {
-        std::lock_guard lock(GetMutex);
+        std::lock_guard lock(Mutex);
         return static_cast<bool>(Instance);
-    }
-
-    static void Destroy()
-    {
-        std::lock_guard lock(GetMutex);
-        Instance.reset();
     }
 };
